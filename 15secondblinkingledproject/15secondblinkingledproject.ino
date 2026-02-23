d6eff26bd58d4120280ef6b39b294943ea93d20e
derekbdev <derekgb84@gmail.com>
2026-02-23 16:40:12 -0500

    Add files via upload

---
+++ 15secondblinkingledproject/15secondblinkingledproject.ino
@@ -0,0 +1,114 @@
+int redLED=11;
+int yellowLED=8;
+int greenLED= 7;
+int blueLED=3;
+int del=1000;
+int dey=1000;
+void setup() {
+ pinMode (yellowLED,OUTPUT); 
+ pinMode(redLED,OUTPUT);
+ pinMode(greenLED,OUTPUT);   
+ pinMode(blueLED,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(yellowLED,HIGH);
+  digitalWrite(redLED,LOW);
+  digitalWrite(greenLED,HIGH);
+  digitalWrite(blueLED,HIGH);
+  delay(del);
+
+  digitalWrite(yellowLED,LOW);
+  digitalWrite(redLED,LOW);
+  digitalWrite(greenLED,HIGH);
+  digitalWrite(blueLED,HIGH);
+  delay(del);
+
+   digitalWrite(yellowLED,HIGH);
+  digitalWrite(redLED,HIGH);
+  digitalWrite(greenLED,HIGH);
+  digitalWrite(blueLED,HIGH);
+  delay(dey);
+
+  digitalWrite(yellowLED,LOW);
+  digitalWrite(redLED,HIGH);
+  digitalWrite(greenLED,HIGH);
+  digitalWrite(blueLED,HIGH);
+  delay(dey);
+  // put your main code here, to run repeatedly:
+digitalWrite(yellowLED,HIGH);
+  digitalWrite(redLED,LOW);
+  digitalWrite(greenLED,LOW);
+  digitalWrite(blueLED,HIGH);
+  delay(del);
+
+   digitalWrite(yellowLED,LOW);
+  digitalWrite(redLED,LOW);
+  digitalWrite(greenLED,LOW);
+  digitalWrite(blueLED,HIGH);
+  delay(dey);
+
+  digitalWrite(yellowLED,HIGH);
+  digitalWrite(redLED,HIGH);
+  digitalWrite(greenLED,LOW);
+  digitalWrite(blueLED,HIGH);
+  delay(dey);
+
+  digitalWrite(yellowLED,LOW);
+  digitalWrite(redLED,HIGH);
+  digitalWrite(greenLED,LOW);
+  digitalWrite(blueLED,HIGH);
+  delay(dey);
+
+  digitalWrite(yellowLED,HIGH);
+  digitalWrite(redLED,LOW);
+  digitalWrite(greenLED,HIGH);
+  digitalWrite(blueLED,LOW);
+  delay(del);
+
+digitalWrite(yellowLED,LOW);
+  digitalWrite(redLED,LOW);
+  digitalWrite(greenLED,HIGH);
+  digitalWrite(blueLED,LOW);
+  delay(del);
+
+ digitalWrite(yellowLED,HIGH);
+  digitalWrite(redLED,HIGH);
+  digitalWrite(greenLED,HIGH);
+  digitalWrite(blueLED,LOW);
+  delay(del);
+ 
+ digitalWrite(yellowLED,LOW);
+  digitalWrite(redLED,HIGH);
+  digitalWrite(greenLED,HIGH);
+  digitalWrite(blueLED,LOW);
+  delay(del);
+ 
+  digitalWrite(yellowLED,HIGH);
+  digitalWrite(redLED,LOW);
+  digitalWrite(greenLED,LOW);
+  digitalWrite(blueLED,LOW);
+  delay(del);
+
+digitalWrite(yellowLED,LOW);
+  digitalWrite(redLED,LOW);
+  digitalWrite(greenLED,LOW);
+  digitalWrite(blueLED,LOW);
+  delay(del);
+
+
+digitalWrite(yellowLED,HIGH);
+  digitalWrite(redLED,HIGH);
+  digitalWrite(greenLED,LOW);
+  digitalWrite(blueLED,LOW);
+  delay(del);
+
+
+   digitalWrite(yellowLED,LOW);
+  digitalWrite(redLED,HIGH);
+  digitalWrite(greenLED,LOW);
+  digitalWrite(blueLED,LOW);
+  delay(del);
+
+
+}
